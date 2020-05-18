f91a72675bf11da48617bd6b981998d1115ac24f
RickyTino <rickytino@qq.com>
2020-05-18 00:58:41 -0400

    Add extra env setting in header

--- include/configs/catnipsoc.h
+++ include/configs/catnipsoc.h
@@ -41,6 +41,13 @@
 #define CONFIG_ENV_SECT_SIZE       0x10000
 #define CONFIG_ENV_OFFSET          0
 //#define CONFIG_ENV_SIZE            0x1000
+#define CONFIG_EXTRA_ENV_SETTINGS \
+    "serverip=192.168.11.1\0" \
+    "ipaddr=192.168.11.2\0" \
+    "netmask=255.255.255.0\0" \
+    "load=tftpboot 0x80100000 uImage;iminfo\0" \
+    "linux=tftpboot 0x80100000 uImage;bootm\0" \
+    "u-boot=tftpboot 0x80000000 u-boot.bin;go 0x80000000\0"
 
 /* ---------------------------------------------------------------------
  * Board boot configuration
